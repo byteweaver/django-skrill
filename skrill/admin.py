3626b1f37b34fcb6f89dff2c89809e9836612df0
akuryou <akuryou@byteweaver.net>
2012-12-04 17:46:58 +0100

    add minimal admin for StatusReport

--- skrill/admin.py
+++ skrill/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from skrill.models import Request
+from skrill.models import Request, StatusReport
 
 
 admin.site.register(Request)
+admin.site.register(StatusReport)
 
