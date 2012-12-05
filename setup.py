1d15ddc816a34f09a33d0df1ca3cb74c0eb7e69f
akuryou <akuryou@byteweaver.net>
2012-12-05 22:27:23 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+import skrill
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-skrill',
+    version=skrill.__version__,
+    description='A reuseable Django application for integrating the Skrill (Moneybookers) payment API',
+    long_description=read('README.md'),
+    license=read('LICENSE'),
+    author='byteweaver',
+    author_email='contact@byteweaver.net',
+    url='https://github.com/byteweaver/django-skrill',
+    packages=find_packages(),
+    install_requires=[
+        'django',
+    ],
+    tests_require=[
+        'django-nose',
+        'coverage',
+        'django-coverage',
+        'factory_boy',
+    ],
+    test_suite='skrill.tests',
+)
