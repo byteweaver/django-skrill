d5dfb37ef62641caafce747fdbd3a60f4d6f0b5b
akuryou <akuryou@byteweaver.net>
2012-12-05 13:25:09 +0100

    remove transaction_id from StatusReportView

--- skrill/views.py
+++ skrill/views.py
@@ -13,7 +13,6 @@ class StatusReportView(View):
         report.pay_from_email = request.POST['pay_from_email']
         report.merchant_id = request.POST['merchant_id']
         report.customer_id = request.POST.get('customer_id', None)
-        report.transaction_id = request.POST['transaction_id']
         report.mb_transaction_id = request.POST['mb_transaction_id']
         report.mb_amount = request.POST['mb_amount']
         report.mb_currency = request.POST['mb_currency']
