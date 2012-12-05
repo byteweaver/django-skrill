9454bd095adb92a8d9ad8facd85146b0070e69c5
akuryou <akuryou@byteweaver.net>
2012-12-05 22:28:48 +0100

    increase version number to 0.1.0

--- skrill/__init__.py
+++ skrill/__init__.py
@@ -3,7 +3,7 @@ import hashlib
 from settings import SECRET_WORD
 
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 
 def get_secret_word_as_md5():
     """returns md5 hash of SECRET_WORD in upper case"""
