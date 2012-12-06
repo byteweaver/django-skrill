4c984369b809ec4376366895f25057780f28231a
akuryou <akuryou@byteweaver.net>
2012-12-06 10:23:41 +0100

    add cancel TemplateView

--- skrill/urls.py
+++ skrill/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import TemplateView
 
 from views import StatusReportView
 
 
 urlpatterns = patterns('',
     url('^status_report/$', csrf_exempt(StatusReportView.as_view()), name='status_report'),
+    url('^cancel/$', TemplateView.as_view(template_name='skrill/cancel.html'), name='cancel'),
 )
