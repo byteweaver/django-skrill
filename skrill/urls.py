224d5312c7ca376a51f033c9f8e4f01189a6c0b0
akuryou <akuryou@byteweaver.net>
2012-12-06 10:29:48 +0100

    add url for return

--- skrill/urls.py
+++ skrill/urls.py
@@ -8,4 +8,5 @@ from views import StatusReportView
 urlpatterns = patterns('',
     url('^status_report/$', csrf_exempt(StatusReportView.as_view()), name='status_report'),
     url('^cancel/$', TemplateView.as_view(template_name='skrill/cancel.html'), name='cancel'),
+    url('^return/$', TemplateView.as_view(template_name='skrill/return.html'), name='return'),
 )
