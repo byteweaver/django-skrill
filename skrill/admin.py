ba528efca0ed3e8507c93a482178e66da98457e2
akuryou <akuryou@byteweaver.net>
2012-12-15 19:50:45 +0100

    add StatusReportAdmin

--- skrill/admin.py
+++ skrill/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from skrill.models import PaymentRequest, StatusReport
 
 
+class StatusReportAdmin(admin.ModelAdmin):
+    list_display = ('time', 'status', 'amount', 'currency', 'valid', 'signal_sent',)
+    list_filter = ('time', 'status', 'valid', 'signal_sent',)
+
 admin.site.register(PaymentRequest)
-admin.site.register(StatusReport)
+admin.site.register(StatusReport, StatusReportAdmin)
 
