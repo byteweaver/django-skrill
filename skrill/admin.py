388a9724dad903ed92253e4d7653cdbe9a0f90ca
akuryou <akuryou@byteweaver.net>
2014-05-12 12:22:30 +0200

    improve StatusReportAdmin

--- skrill/admin.py
+++ skrill/admin.py
@@ -9,8 +9,9 @@ class PaymentRequestAdmin(admin.ModelAdmin):
     search_fields = ('user__email', 'user__username', 'user__first_name', 'user__last_name')
 
 class StatusReportAdmin(admin.ModelAdmin):
-    list_display = ('time', 'status', 'amount', 'currency', 'valid', 'signal_sent',)
-    list_filter = ('time', 'status', 'valid', 'signal_sent',)
+    list_display = ('time', 'status', 'amount', 'currency', 'mb_transaction_id', 'valid', 'signal_sent',)
+    list_filter = ('time', 'status', 'valid', 'signal_sent')
+    search_fields = ('payment_request__user__email', 'payment_request__user__username', 'payment_request__user__first_name', 'payment_request__user__last_name', 'mb_transaction_id')
 
 admin.site.register(PaymentRequest, PaymentRequestAdmin)
 admin.site.register(StatusReport, StatusReportAdmin)
