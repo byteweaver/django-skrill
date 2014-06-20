5a0db5428fcf4beae2d775db6514cdc15932b684
akuryou <akuryou@byteweaver.net>
2014-06-20 15:10:02 +0200

    use generate_md5_signature function from factory

--- skrill/tests/models_tests.py
+++ skrill/tests/models_tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 
 from skrill.models import StatusReport
-from skrill.tests.factories import StatusReportFactory
+from skrill.tests.factories import StatusReportFactory, generate_md5_signature
 
 
 class StatusReportTestCase(TestCase):
     def test_valid_md5_signature(self):
         status_report = StatusReportFactory()
-        status_report.md5sig = status_report.generate_md5_signature()
+        status_report.md5sig = generate_md5_signature(status_report)
         status_report.validate_md5sig()
 
     def test_invalid_md5_signature(self):
